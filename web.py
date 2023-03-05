eb2d21889c4165e97619263a5d4442bb5fcc1378
sreejith <squeeky.mouse@gmail.com>
2023-03-06 01:14:42 +0530

    scrap temp into sqlite db n plot on webpage Sec38

--- web.py
+++ web.py
@@ -5,14 +5,21 @@ import sqlite3
 
 st.title('Temperature Plot')
 
-# df=pd.read_csv('data.txt')
-
 dbconn=sqlite3.connect('data.sqlite')
 cursor=dbconn.cursor()
+
 cursor.execute("SELECT * FROM temp")
 rows=cursor.fetchall()
 temp=[row[1] for row in rows]
 date=[row[0] for row in rows]
+
+# Same as above
+# cursor.execute("SELECT date FROM temp")
+# date=cursor.fetchall()
+# date=[i[0] for i in date]
+# cursor.execute("SELECT date FROM temp")
+# temp=cursor.fetchall()
+# temp=[i[1] for i in temp]
 
 plot=px.line(x=date,y=temp,labels={'x':'Date','y':'Temperature (C)'})
 st.plotly_chart(plot)
